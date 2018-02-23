5e926d0bf06fb9d0170fe0227485242b2b53385b
Shiva Kumar Madhavan <shiva.madhavan@gmail.com>
2018-02-23 13:11:44 +0800

    style: fix lint errors

--- red-utils.js
+++ red-utils.js
@@ -1,6 +1,6 @@
 let ck = require('check-types');
 console.log('Hello Semantic Release. This is the first automated release.');
-//@todo add feature 3
-//@body Implement featre 3 for next release
+// @todo add feature 3
+// @body Implement featre 3 for next release
 ck;
 console.log('A new feature 2');
